2b4f2cb17e68278f99c56ee72a3f8db0662151a2
phanDinhDan <phandinhdan6602@gmail.com>
2024-05-23 14:35:34 +0700

    anh khạc hay em khạc

---
+++ app/src/main/java/com/dinhdan/prm392_ex8/Animal.java
@@ -0,0 +1,21 @@
+package com.dinhdan.prm392_ex8;
+
+public class Animal {
+    private int id;
+    private String name;
+    private String description;
+    public Animal(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
